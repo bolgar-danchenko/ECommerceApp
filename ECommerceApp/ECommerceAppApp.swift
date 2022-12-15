7ee44c68a7fb750ac86a883e1d685f84cbe3cf6d
Konstantin Bolgar-Danchenko <constantine.bolgar@icloud.com>
2022-12-15 13:05:57 +0400

    Initial Commit

---
+++ ECommerceApp/ECommerceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ECommerceAppApp.swift
+//  ECommerceApp
+//
+//  Created by Konstantin Bolgar-Danchenko on 15.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ECommerceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
